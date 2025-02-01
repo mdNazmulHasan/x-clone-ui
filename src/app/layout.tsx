a718b998bfc334c29aaf59b84b996bbec7a025bf
mdNazmulHasan <najmul.hasan10@gmail.com>
2025-02-01 15:14:48 +0600

    Add LeftBar and RightBar components to layout; enhance styling and structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import LeftBar from "@/components/LeftBar";
 import "./globals.css";
+import RightBar from "@/components/RightBar";
 
 export default function RootLayout({
   children,
@@ -9,10 +11,14 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="max-w-screen-md lg:max-w-screen-lg mx-auto flex justify-between">
-          <div className="col-span-1">LeftBar</div>
-          <div className="col-span-1">{children}</div>
-          <div className="col-span-1">RightBar</div>
-        </div> 
+          <div className="px-2 xsm:px-4 2xl:px-8 bg-red-200 h-screen">
+            <LeftBar />
+          </div>
+          <div className="flex-1 bg-green-200 h-screen">{children}</div>
+          <div className="hidden lg:flex ml-4 md:ml-8 bg-blue-200 h-screen">
+            <RightBar />
+          </div>
+        </div>
       </body>
     </html>
   );
